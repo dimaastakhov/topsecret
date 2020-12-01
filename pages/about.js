fa31675126138769d8548e8c4f841141d8981c21
irakliCanuyvadze <iraklichanuyvadze@gmail.com>
2020-12-01 15:21:41 +0400

    responsive about

--- pages/about.js
+++ pages/about.js
@@ -52,43 +52,47 @@ export default function about() {
               quality={100}
             />
           </div>
-          <div className="max-w-lg ml-auto mr-28">
-            <div>
-              <h2 className="text-8.5 md:text-10 leading-13  text-fff tracking-wider">
-                Смотрите “TOPSECRET”
-              </h2>
-              <h3 className="text-8.5 md:text-10 leading-13 text-fff tracking-wider">
-                из любой точки мира
-              </h3>
-              <p className="text-xs lg:text-sm  text-fff opacity-50 w-88 lg:w-140 ">
-                Давно выяснено, что при оценке дизайна и композиции читаемый
-                текст мешает сосредоточиться. используют потому, что тот
-                обеспечивает более или менее стандартное заполнение шаблона, а
-                также реальное
-              </p>
-              <button>
-                <div className="w-40 h-12 flex justify-center items-center bg-137BFF rounded-md mt-7.5 ">
-                  <div className="mr-2.5">
-                    <Image
-                      width="24"
-                      height="24"
-                      alt="play"
-                      src="/play-tr.svg"
-                      layout="fixed"
-                      quality={100}
-                    />
+          <div className="max-w-lg ml-auto  mr-auto sm:mr-28 ">
+            <div className="flex flex-col space-y-48 h-102 sm:h-auto sm:space-y-0">
+              <div className="mt-7 sm:mt-0">
+                <h2 className="text-8.5 md:text-10 leading-13  text-fff tracking-wider">
+                  Смотрите “TOPSECRET”
+                </h2>
+                <h3 className="text-8.5 md:text-10 leading-13 text-fff tracking-wider">
+                  из любой точки мира
+                </h3>
+                <p className="text-xs lg:text-sm  text-fff opacity-50 w-88 lg:w-140 ">
+                  Давно выяснено, что при оценке дизайна и композиции читаемый
+                  текст мешает сосредоточиться. используют потому, что тот
+                  обеспечивает более или менее стандартное заполнение шаблона, а
+                  также реальное
+                </p>
+              </div>
+              <div>
+                <button className="w-full sm:w-40">
+                  <div className="w-full sm:w-40 h-12 flex justify-center items-center bg-137BFF rounded-md mt-7.5 ">
+                    <div className="mr-2.5 mt-1">
+                      <Image
+                        width="24"
+                        height="24"
+                        alt="play"
+                        src="/play-tr.svg"
+                        layout="fixed"
+                        quality={100}
+                      />
+                    </div>
+                    <div>
+                      <p className="text-sm text-fff">Смотреть</p>
+                    </div>
                   </div>
-                  <div>
-                    <p className="text-sm text-fff">Смотреть</p>
-                  </div>
-                </div>
-              </button>
+                </button>
+              </div>
             </div>
           </div>
         </div>
-        <div className="relative mt-22 w-full flex">
-          <div className=" w-1/2 md:w-2/3">
-            <div className="ml-10  w-4/6 md:w-1/2 ">
+        <div className="relative mt-22 w-full flex flex-col sm:flex-row">
+          <div className="w-full sm:w-1/2 md:w-2/3">
+            <div className="ml-0 sm:ml-10  w-full sm:w-4/6 md:w-1/2 ">
               <div>
                 <h4 className="text-2x1 text-fff leading-7">
                   Давайте работать вместе{" "}
@@ -119,19 +123,25 @@ export default function about() {
                 <div className="w-1/2 mt-12.5">
                   <h5 className="text-sm text-fff leading-5.25">Адрес</h5>
                   <p className="text-sm text-5D5D5E leading-5.5  mt-4">
-                    Tbilisi, Georgia Улица Давида Агмашенебели, 24
+                    Tbilisi, Georgia
+                  </p>
+                  <p className="text-sm text-5D5D5E leading-5.5  mt-4">
+                    Улица Давида Агмашенебели, 24
                   </p>
                 </div>
                 <div className="w-1/2 mt-12.5">
                   <h5 className="text-sm text-fff leading-5.25">Почта</h5>
                   <p className="text-sm text-5D5D5E leading-4 mt-4">
-                    Info@topsecret.com Support@topsecret.com
+                    Info@topsecret.com
+                  </p>
+                  <p className="text-sm text-5D5D5E leading-4 mt-4">
+                    Info@topsecret.com
                   </p>
                 </div>
               </div>
             </div>
           </div>
-          <div className="w-1/2 md:w-1/3 pr-10 ">
+          <div className="w-full sm:w-1/2 md:w-1/3 pr-0 sm:pr-10 mt-22 sm:mt-0 ">
             <div className="mb-17.5">
               <h4 className="text-2x1 text-fff leading-7">Напишите нам !</h4>
               <p className="text-sm text-fff leading-6 opacity-30 mt-4">
@@ -141,19 +151,19 @@ export default function about() {
               </p>
             </div>
             <div className="flex mb-17 flex-col xl:flex-row">
-              <div className="w-1/2">
+              <div className="w-full xl:w-1/2">
                 <input
-                  className="bg-black pb-2 text-545454 border-b-2 border-gray-600 mt-4 "
+                  className="bg-lightGray pb-1 pt-1 text-545454 border-b-2 border-gray-600 mt-4 w-full xl:w-2/3 "
                   type="text"
                   placeholder="имя"
                 />
               </div>
-              <div className="w-1/2 mt-17 xl:mt-0">
+              <div className="w-full xl:w-1/2 mt-17 xl:mt-0">
                 <div className="h-4">
                   <p className="text-xs text-545454 leading-3.5">Эл. почта</p>
                 </div>
                 <input
-                  className="bg-black pb-2 text-fff border-b-2 border-gray-600 "
+                  className="bg-lightGray pb-1 pt-1 text-fff border-b-2 border-gray-600 w-full  xl:w-2/3   "
                   type="text"
                   value="Info@topsecret.com"
                 />
@@ -161,13 +171,13 @@ export default function about() {
             </div>
             <div className="w-full mb-9">
               <input
-                className="bg-black pb-2 text-545454 border-b-2 border-gray-600 w-full "
+                className="bg-lightGray pb-1 pt-1  text-545454 border-b-2 border-gray-600 w-full  "
                 type="text"
                 placeholder="Подробное описание"
               />
             </div>
-            <button>
-              <div className="w-40 h-12 flex justify-center items-center bg-137BFF rounded-md mt-7.5 text-sm text-fff ">
+            <button className="w-full sm:w-40">
+              <div className="w-full sm:w-40 h-12 flex justify-center items-center bg-137BFF rounded-md mt-7.5 text-sm text-fff ">
                 Отправить
               </div>
             </button>
